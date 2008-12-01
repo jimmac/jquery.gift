c894caf3d69921e78acc7ef746ac9a9443d42e96
Jakub Steiner <jimmac@gmail.com>
2008-12-01 23:40:37 +0100

    one loop working.

--- jquery.gift.js
+++ jquery.gift.js
@@ -2,13 +2,37 @@
 // (c) Jakub 'jimmac Steiner
 //
 
-(function () {$.fn.gift = function () {
-  var blank = "data:image/gif;base64,R0lGODlhFAAUALMAAAAAAIAAAACAAICAAAAA
-                gIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//////yH5BAEAAA8AL
-                AAAAAAUABQAQAQV8MlJq7046827/2AojmRpnmiqrmYEADs=";
-  $(this).each(function () {
-    //replace the image with a blank we'll animate
-    //
-  });
-  return this;
-}}) (jQuery);
+(function ($) {
+  $.fn.gift = function () {
+    var blank = "data:image/gif;base64,R0lGODlhFAAUALMAAAAAAIAAAACAAICAAAAA\
+                  gIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//////yH5BAEAAA8AL\
+                  AAAAAAUABQAQAQV8MlJq7046827/2AojmRpnmiqrmYEADs=";
+    $(this).each(function () {
+      //replace the image with a blank we'll animate
+
+      var img = Object;
+      img.width = $(this).attr('width');
+      img.height = $(this).attr('height');
+      img.delay = 100; //100ms by default
+      img.src = $(this).attr('src');
+      $(this).attr('src',blank).css('background','url(' + img.src + ') no-repeat 0 0');
+      //FIXME: use actual image height and assume square if no explicit attributes exist.
+      //FIXME: need actual image width to get max offset value
+      //FIXME: read delay from the rel
+      var image = this; //needs to be the actual DOM object
+      setInterval(function() {
+        var offset = $(image).css('background-position'); //x y in px
+        var offset_x = offset.match(/([-]?[0-9]*)px ([-]?[0-9]*)px/)[1]; //integer
+        var newoffset_x = offset_x - img.width;
+        //FIXME: check for max offset
+        $(image).css('background-position', newoffset_x + "px 0");
+      },img.delay);
+    });
+    return this;
+  }
+}) (jQuery)
+
+//actually use the plugin on the document
+$(document).ready(function() {
+  $("img[rel*=animated]").gift();
+});
