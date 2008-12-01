7cf450bcb15c8c8e75f3110c71eb5a4197ea1af5
Jakub Steiner <jimmac@gmail.com>
2008-12-01 22:25:06 +0100

    embedded blank.gif and sample documentation

--- jquery.gift.js
+++ jquery.gift.js
@@ -1,2 +1,14 @@
 // GIFT -- use PNG RGBA tiled images as animations
 // (c) Jakub 'jimmac Steiner
+//
+
+(function () {$.fn.gift = function () {
+  var blank = "data:image/gif;base64,R0lGODlhFAAUALMAAAAAAIAAAACAAICAAAAA
+                gIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//////yH5BAEAAA8AL
+                AAAAAAUABQAQAQV8MlJq7046827/2AojmRpnmiqrmYEADs=";
+  $(this).each(function () {
+    //replace the image with a blank we'll animate
+    //
+  });
+  return this;
+}}) (jQuery);
