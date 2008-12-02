78f577134dc5f98d22d607d907df081d21e90bd8
Garrett LeSage <garrett@novell.com>
2008-12-02 11:11:49 +0100

    first pass at a quick JS refactor (more to come soon!)

--- jquery.gift.js
+++ jquery.gift.js
@@ -1,34 +1,44 @@
-// GIFT -- use PNG RGBA tiled images as animations
-// (c) Jakub 'jimmac Steiner
-//
+// GIFT -- a jQuery plugin to use PNG RGBA tiled images as animations
+// (c) 2008 Jakub 'jimmac' Steiner & Garrett LeSage
 
 (function ($) {
-  $.fn.gift = function () {
+  $.fn.gift = function() {
     var blank = "data:image/gif;base64,R0lGODlhFAAUALMAAAAAAIAAAACAAICAAAAA\
                   gIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//////yH5BAEAAA8AL\
                   AAAAAAUABQAQAQV8MlJq7046827/2AojmRpnmiqrmYEADs=";
-    $(this).each(function () {
+    $(this).each(function() {
+      var width, height, img, image, offset, offset_x, newoffset_x;
+
       //replace the image with a blank we'll animate
-      var width = $(this).width();
-      var height = $(this).height(); //if hieght attr is omitted, this returns bogus
-      //load 
-      var img = new Image(); //create a new image for measuring real dimenstions
+      width = $(this).width();
+      height = $(this).height(); //if height attr is omitted, this is bogus
+
+      img = new Image(); //create a new image for measuring real dimenstions
       img.delay = 100; //100ms by default
       img.src = $(this).attr('src');
+
       $(img).attr("style","display: none;");
-      $(this).after(img,function() { console.log('appended');});
-      $(this).attr('src',blank).css({background: 'url(' + img.src + ') no-repeat 0 0', 
-                                     width: width, height: height});
+
+      $(this).after(img, function(){ console.log('appended');});
+
+      $(this).attr('src', blank).css({
+        background: 'url(' + img.src + ') no-repeat 0 0',
+        width: width,
+        height: height
+      });
+
       //FIXME: read delay from the rel
-      var image = this; //needs to be the actual DOM object
+      image = this; //needs to be the actual DOM object
+
       setInterval(function() {
-        var offset = $(image).css('background-position'); //x y in px
-        var offset_x = offset.match(/([-]?[0-9]*)px ([-]?[0-9]*)px/)[1]; //integer
-        var newoffset_x = offset_x - width;
+        offset = $(image).css('background-position'); //x y in px
+        offset_x = offset.match(/([-]?[0-9]*)px ([-]?[0-9]*)px/)[1]; //integer
+        newoffset_x = offset_x - width;
         //FIXME: check for max offset
         newoffset_x = (newoffset_x<=-img.width)? 0 : newoffset_x;
         $(image).css('background-position', newoffset_x + "px 0");
-      },img.delay);
+      }, img.delay);
+
       $(img).remove();//remove the temp image 
     });
     return this;
